06e35ac931b1f7e8cdc99ab9c4c2938ee8d9d69d
irfan-007 <thanatos786687@gmail.com>
2024-09-17 15:14:06 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,38 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
+import "./../styles/App.css";
 
 const App = () => {
+  const [todo, setTodo] = useState([]);
+  const [task, setTask] = useState("");
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h1>To-Do List</h1>
+      <input
+        type="text"
+        name="add"
+        id="add"
+        placeholder="Enter the task"
+        onChange={(e) => setTask(e.target.value)}
+      />
+      <button onClick={() => setTodo([...todo, task])}>Add Todo</button>
+      <ul>
+        {todo.map((elem, i) => (
+          <li>
+            {elem}{" "}
+            <button
+              onClick={() => {
+                let tem = [...todo];
+                tem.splice(i, 1);
+                setTodo(tem);
+              }}
+            >
+              Delete
+            </button>
+          </li>
+        ))}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
